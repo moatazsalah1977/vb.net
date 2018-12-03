08c74c3140abf1d0d2eb93a740b71f1fb8c4a986
Moataz Salah <40663860+moatazsalah@users.noreply.github.com>
2018-12-03 17:40:19 +0300

    initial commit

---
+++ DataGridView Format.vb
@@ -0,0 +1,21 @@
+
+sub dgv_mystyle
+'adjusting Datagrid view
+        datagridview1.rowsheadervisable = false
+        DataGridView1.GridColor = Color.blue
+        DataGridView1.CellBorderStyle = DataGridViewCellBorderStyle.None
+        DataGridView1.BackgroundColor = Color.white
+
+'Adjusting rows
+        datagridview1.rowsheadervisable = false
+        DataGridView1.RowsDefaultCellStyle.BackColor = Color.Bisque
+        DataGridView1.AlternatingRowsDefaultCellStyle.BackColor = Color.gray
+End Sub
+
+'cell selection
+        DataGridView1.DefaultCellStyle.SelectionBackColor = Color.Red
+        DataGridView1.DefaultCellStyle.SelectionForeColor = Color.Yellow
+
+        DataGridView1.DefaultCellStyle.WrapMode = DataGridViewTriState.[True]
+        DataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect
+       
